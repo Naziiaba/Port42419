4b13a7e9592f23e7d195d4121ab3492bcb2efbbd
Naziia Travels <35944002+Naziiaba@users.noreply.github.com>
2019-04-26 13:59:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){                 // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({                  // Using jQuery's animate() method to add smooth page scroll
+        scrollTop: $(hash).offset().top
+      }, 900, function(){                       // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
